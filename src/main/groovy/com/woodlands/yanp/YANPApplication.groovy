bd26eff683ce7289f12f18b1e73e6e90cf04808c
Badger <BadgerByNature@gmail.com>
2026-02-09 22:57:14 -0600

    Initial commit

---
+++ src/main/groovy/com/woodlands/yanp/YANPApplication.groovy
@@ -0,0 +1,12 @@
+package com.woodlands.yanp
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class YANPApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(YANPApplication, args)
+	}
+}

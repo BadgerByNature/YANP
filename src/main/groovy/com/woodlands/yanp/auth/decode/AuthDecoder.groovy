bb705709fd17e2699ad34837c485eac933be9fec
Badger <BadgerByNature@gmail.com>
2026-02-11 23:59:52 -0600

    Log when we don't find the decoder and reset the byteBuffer reader index

--- src/main/groovy/com/woodlands/yanp/auth/decode/AuthDecoder.groovy
+++ src/main/groovy/com/woodlands/yanp/auth/decode/AuthDecoder.groovy
@@ -2,6 +2,7 @@ package com.woodlands.yanp.auth.decode
 
 import com.woodlands.yanp.auth.AuthCommand
 import com.woodlands.yanp.auth.AuthCommandDecoder
+import groovy.util.logging.Slf4j
 import io.netty.buffer.ByteBuf
 import io.netty.channel.ChannelHandlerContext
 import io.netty.handler.codec.ByteToMessageDecoder
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 // TODO Can this be Spring-managed or do we need one per thread? Can we multithread it with Java 25 features?
+@Slf4j
 @Service
 class AuthDecoder extends ByteToMessageDecoder {
 
@@ -25,13 +27,15 @@ class AuthDecoder extends ByteToMessageDecoder {
         // Marks the reader index in case it needs to be reset - when could that happen?
         byteBuf.markReaderIndex()
 
-
         byte commandCode = byteBuf.readByte()
 
         AuthCommand command = AuthCommand.fromCode(commandCode)
         var decoder = authCommandDecoders.find { it -> it.handles(command) }
         if (decoder != null) {
             listOut.add(decoder.decode(byteBuf))
+        } else {
+            log.error("No decoder found for packet with command code ${Integer.toHexString(commandCode)}")
+            byteBuf.resetReaderIndex()
         }
     }
 }

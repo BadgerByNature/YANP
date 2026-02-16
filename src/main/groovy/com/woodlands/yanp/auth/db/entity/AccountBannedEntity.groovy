2a18a30e19dbcddd4036160217b16a18a9d69474
Badger <BadgerByNature@gmail.com>
2026-02-15 21:21:24 -0600

    YANP-6 Properly annotate Entity

--- src/main/groovy/com/woodlands/yanp/auth/db/entity/AccountBannedEntity.groovy
+++ src/main/groovy/com/woodlands/yanp/auth/db/entity/AccountBannedEntity.groovy
@@ -1,11 +1,13 @@
 package com.woodlands.yanp.auth.db.entity
 
 import jakarta.persistence.Column
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 
+@Entity
 @Table(name = 'account_banned', catalog = 'tbcrealmd')
 class AccountBannedEntity {
 

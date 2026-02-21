c2025dcad510dadb668de76a7a27b5e040bc2b35
Badger <BadgerByNature@gmail.com>
2026-02-20 18:23:47 -0600

    YANP-11 Set the random to be final

--- src/main/groovy/com/woodlands/yanp/common/srp/WowSrpService.groovy
+++ src/main/groovy/com/woodlands/yanp/common/srp/WowSrpService.groovy
@@ -32,7 +32,7 @@ class WowSrpService {
     public static final byte[] VERSION_CHALLENGE =
             [ 0xBA, 0xA3, 0x1E, 0x99, 0xA0, 0x0B, 0x21, 0x57, 0xFC, 0x37, 0x3F, 0xB3, 0x69, 0xCD, 0xD2, 0xF1 ]
 
-    SecureRandom secureRandom
+    final SecureRandom secureRandom
 
     WowSrpService(SecureRandom secureRandom) {
         this.secureRandom = secureRandom

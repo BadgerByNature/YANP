fc34d7c06e7d3c20eff835ccf0b8c1330e901878
Badger <BadgerByNature@gmail.com>
2026-02-23 23:52:03 -0600

    YANP-12 Update RealmList Handler to use real database values for character count per realm. Update timezone logic to more closely match CMangos even though it results in the same value for most reasonable cases. Check account security level to avoid displaying realms the player should not have access to. Send a count of /eligible/ realms even if more realms will actually be displayed as locked (per CMangos) Cleanup comments and todos where possible

--- src/main/groovy/com/woodlands/yanp/auth/message/handler/RealmListMessageHandler.groovy
+++ src/main/groovy/com/woodlands/yanp/auth/message/handler/RealmListMessageHandler.groovy
@@ -15,11 +15,13 @@
  * Copyright (c) 2026 YANP: You Are Not Prepared
  * See CONTRIBUTORS.md for further Copyright information
  */
+//file:noinspection GrMethodMayBeStatic
 package com.woodlands.yanp.auth.message.handler
 
 import com.woodlands.yanp.auth.AuthCommand
 import com.woodlands.yanp.auth.AuthServer
 import com.woodlands.yanp.auth.db.entity.RealmEntity
+import com.woodlands.yanp.auth.db.repository.RealmCharactersRepository
 import com.woodlands.yanp.auth.db.repository.RealmRepository
 import com.woodlands.yanp.auth.message.AuthMessage
 import com.woodlands.yanp.auth.message.RealmListMessage
@@ -34,9 +36,13 @@ import org.springframework.stereotype.Service
 @Service
 class RealmListMessageHandler implements AuthMessageHandler {
 
+    final static int MAX_REALM_ZONES = 38 // Max timezone value for a realm in WotLK
+
+    final RealmCharactersRepository realmCharactersRepository
     final RealmRepository realmRepository
 
-    RealmListMessageHandler(RealmRepository realmRepository) {
+    RealmListMessageHandler(RealmCharactersRepository realmCharactersRepository, RealmRepository realmRepository) {
+        this.realmCharactersRepository = realmCharactersRepository
         this.realmRepository = realmRepository
     }
 
@@ -44,37 +50,50 @@ class RealmListMessageHandler implements AuthMessageHandler {
     void handle(AuthMessage message, Channel ch) {
         log.debug('Handling RealmListMessage')
 
-//        def account = ch.attr(AuthServer.ACCOUNT).get() // TODO get info from account once we're satisfied this is working
+        def account = ch.attr(AuthServer.ACCOUNT).get()
+        if (!account) {
+            throw new Exception("Account not found on Realm List message")
+        }
 
+        // Get the character counts for all realms up front instead of opening a query for each realm individually
+        def realmCharacters = realmCharactersRepository.findByAccountId(account.id)
         // TODO Realm list cache instead of calling the table on every access
         def realms = realmRepository.findAll()
-        log.debug('Realm findAll() completed')
 
-        def realmWriter = new PacketDataWriter()
+        def realmInfosWriter = new PacketDataWriter()
         for (RealmEntity realm : realms) {
-            realmWriter.writeByte(realm.icon)
-            realmWriter.writeByte(0) // TODO Determine if realm is 'locked' and write it here - Only GMs can see realms that have a higher security level than they can access, those realms display as locked
-            realmWriter.writeByte(realm.realmFlags)
-            realmWriter.writeNullTerminatedAsciiString(realm.name)
-            realmWriter.writeNullTerminatedAsciiString("$realm.address:$realm.port")
-            realmWriter.writeFloatLE(realm.population)
-            realmWriter.writeByte(5) // TODO Num Characters from realmcharacters table by realmid and acctid (defaulting to 5 for testing)
-            realmWriter.writeByte(realm.timezone) // TODO Realm Zone from Build + timezone? See CMangos
-            realmWriter.writeByte(0x2c) // TODO TBC sends Realm Id here, Vanilla sends 0x00
-            // TODO Get real Build info - not required?
+            // Players can't see any realms with increased security levels
+            if (account.gmLevel == 0 && realm.allowedSecurityLevel > 0) continue
+            // GMs can apparently see realms at a higher security level than themselves in the list, but these realms show as locked
+            def locked = realm.allowedSecurityLevel > account.gmLevel
+            // Get the character count for this account at this specific realm
+            def characterCount = realmCharacters.find { it.realmId == realm.id }.with { it.count }
+
+            realmInfosWriter.writeByte(realm.icon)
+            realmInfosWriter.writeByte(locked)
+            realmInfosWriter.writeByte(realm.realmFlags)
+            realmInfosWriter.writeNullTerminatedAsciiString(realm.name)
+            realmInfosWriter.writeNullTerminatedAsciiString("$realm.address:$realm.port")
+            realmInfosWriter.writeFloatLE(realm.population)
+            realmInfosWriter.writeByte(characterCount)
+            realmInfosWriter.writeByte(getRealmZoneFromTimezone(realm.timezone)) // This is really the realm zone or realm category, e.g. US or Oceanic
+            realmInfosWriter.writeByte(0x2c) // CMangos just always writes 0x2c. ACore writes field[0]'s id, which sounds like the database id - that seems like info the client should not see
+            // TODO Get real Build info - required when realmflags include 0x04 'SPECIFYBUILD'
 //            realmWriter.writeByte(0x02) // Realm major version
 //            realmWriter.writeByte(0x04) // Realm minor version
 //            realmWriter.writeByte(0x03) // Patch version
 //            realmWriter.writeShortLE(8606) // Build number/identifier
         }
-        realmWriter.writeShortLE(0x0010)
+        realmInfosWriter.writeShortLE(0x0010)
 
         def payloadWriter = new PacketDataWriter()
-        payloadWriter.writeShortLE(realmWriter.getBytes().length + 6) // realm buffer size plus unused int and realms count as short (below)
+        /* We write the packet size up front, and it is equal to the size of the realmInfos content,
+           plus a seemingly-unused int (4 bytes), plus a short representing number of realms sent (2 bytes) */
+        payloadWriter.writeShortLE(realmInfosWriter.getBytes().length + 4 + 2) // Payload size as short
         payloadWriter.writeIntLE(0) // Unused int (4 bytes)
-        payloadWriter.writeShortLE(realms.size()) // Realm size short (2 bytes)
-        payloadWriter.write(realmWriter.getBytes())
-        log.debug("Realm List: ${payloadWriter.bytes.encodeHex()}")
+        payloadWriter.writeShortLE(countEligibleRealms(account.gmLevel, realms)) // Total eligible realm count short (2 bytes) - may not match realms.size
+        payloadWriter.write(realmInfosWriter.getBytes()) // All the realm information
+
         ch.writeAndFlush(new ByteBufWowPacket(
                 opCode: AuthCommand.CMD_REALM_LIST.code,
                 payload: Unpooled.wrappedBuffer(payloadWriter.bytes))
@@ -84,6 +103,36 @@ class RealmListMessageHandler implements AuthMessageHandler {
     @Override
     boolean handles(AuthMessage message) {
         message instanceof RealmListMessage
+    }
+
+    /**
+     * Returns the realm zone from the value stored in the account `timezone` field. The logic for this varies by
+     * expansion. A realm zone represents a sort of region, such as US, German, or Oceanic
+     *
+     * @param timezone
+     * @return realmZone identifier for the given timezone
+     */
+    int getRealmZoneFromTimezone(int timezone) {
+        if (timezone > MAX_REALM_ZONES) return 1
+        // Vanilla would have some special handling here. Wrath just uses the timezone values (if <= MAX_REALM_ZONE)
+        // TBC uses the timezone value up until entry 31
+        // Also 'timezone' isn't accurate, it's more Realm Zone, e.g. United States, or Oceanic
+        timezone in 0..30 ? timezone : 0
+    }
+
+    /**
+     * Returns the number of realms this account is eligible for. This may not match the number of accounts
+     * sent to the client. GMs can apparently see realms with higher required security, but they show as locked.
+     *
+     * @param gmLevel securityLevel of the account, also called gmLevel
+     * @param realmList the list of realms
+     * @return count of realms this user can log into in the realmList
+     */
+    int countEligibleRealms(int gmLevel, List<RealmEntity> realmList) {
+         if (gmLevel == 0 ) {
+             return realmList.count { it.allowedSecurityLevel == 0 }
+         }
+        return realmList.count { it.allowedSecurityLevel <= gmLevel }
     }
 }
 

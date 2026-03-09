0c80998afe1e7eaa3d67dc8542afc4bca6927486
Badger <BadgerByNature@gmail.com>
2026-03-08 23:21:23 -0500

    Don't worry about this potential over-optimization at this time

--- src/main/groovy/com/woodlands/yanp/auth/db/repository/AccountBannedRepository.groovy
+++ src/main/groovy/com/woodlands/yanp/auth/db/repository/AccountBannedRepository.groovy
@@ -21,8 +21,6 @@ import com.woodlands.yanp.auth.db.entity.AccountBannedEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AccountBannedRepository extends JpaRepository<AccountBannedEntity, Long> {
-    // TODO CMangos only pulls the banned_at and expires_at columns - is that actually more performant? Enough that we should we replicate that behavior?
-    // It's a lot trickier to do cleanly in JPA https://www.w3docs.com/snippets/java/spring-jpa-selecting-specific-columns.html
 
     AccountBannedEntity findByIdAndIsActive(Integer id, boolean active)
 }
